e30845c911c41ef14ade78bdaac3b01ffa726594
Linards Lazdiņš <linards_lazdins@hotmail.com>
2024-11-04 07:54:41 +0200

    Add seeder for users and admin dashboard route

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,9 @@
+<x-layout.admin>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Admin Dashboard</h1>
+            </div>
+        </div>
+    </div>
+</x-layout.admin>
