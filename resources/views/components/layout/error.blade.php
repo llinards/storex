f60769b181e3c1d6e172f74c7b5695aac935a206
Linards Lazdiņš <linards_lazdins@hotmail.com>
2024-11-05 09:17:12 +0200

    Add logo to error layout

--- resources/views/components/layout/error.blade.php
+++ resources/views/components/layout/error.blade.php
@@ -14,9 +14,8 @@
 </head>
 <body class="min-vh-100">
 <main class="d-flex justify-content-center align-items-center flex-column mx-2 min-vh-100">
-    <div class="text-center mb-2">
-        {{--        TODO: Add logo here--}}
-        <img src="{{ asset('storage/logo/logo-black.png') }}" class="img-fluid w-25" alt="Storex Structures logo">
+    <div class="text-center mb-4">
+        <img src="{{ asset('images/storex-logo.png') }}" class="img-fluid w-25" alt="Storex Structures logo">
     </div>
     {{ $slot }}
 </main>
