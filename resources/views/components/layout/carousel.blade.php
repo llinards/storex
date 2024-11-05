448931ed8a388a3505a7d8e65ed5c6652dabb1b2
KarlisEmbergs <karlisembergs@gmail.com>
2024-11-05 20:54:04 +0200

    Stored carousel as a component, added some additional CSS

---
+++ resources/views/components/layout/carousel.blade.php
@@ -0,0 +1,36 @@
+<div class="container" id="carousel">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active d-flex align-items-center"
+                        style="background-image:url({{ asset('images/placeholder-tent-1.jpg') }})">
+                        <div class="container col-9">
+                            <div class="col-sm-7 text-center text-sm-start">
+                                <h1>Tenta angāri Jūsu saimniecībai vai uzņēmumam!</h1>
+                                <p>Uzticamība un augsta kvalitāte</p>
+                                <button class="btn btn-primary">Uz produkciju</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
