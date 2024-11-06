b3a0feac1b3986a0a0f0b0e18c40f1ba6988b278
KarlisEmbergs <karlisembergs@gmail.com>
2024-11-06 22:48:31 +0200

    Fixed carousel related issues with alignment and how classes affect functionality. Some additional CSS as well.

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,36 +1,62 @@
 <div class="container" id="header-carousel">
     <div class="row">
         <div class="col-md-12 p-0">
-            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+
+            <div id="carousel-autoplay" class="carousel slide">
                 <div class="carousel-inner">
-                    <div class="carousel-item active d-flex align-items-center"
+
+                    <div class="carousel-item active"
                         style="background-image:url({{ asset('images/placeholder-tent-1.jpg') }})">
-                        <div class="container col-9">
-                            <div class="col-sm-12 text-center text-sm-start">
-                                <h1>Tenta angāri Jūsu saimniecībai vai uzņēmumam!</h1>
-                                <p>Uzticamība un augsta kvalitāte</p>
-                                <button class="btn btn-primary">Uz produkciju</button>
+                        <div class="container h-100 text-break">
+                            <div class="row mx-sm-5 h-100 align-items-center">
+                                <div class="col mx-sm-5 text-sm-start text-center">
+                                    <h1>Tenta angāri Jūsu saimniecībai vai uzņēmumam!</h1>
+                                    <p>Uzticamība un augsta kvalitāte</p>
+                                    <button class="btn btn-primary">Uz produkciju</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="...">
+
+                    <div class="carousel-item"
+                        style="background-image:url({{ asset('images/placeholder-tent-1.jpg') }})">
+                        <div class="container h-100 text-break">
+                            <div class="row mx-sm-5 h-100 align-items-center">
+                                <div class="col mx-sm-5 text-sm-start text-center">
+                                    <h1>Test 2!</h1>
+                                    <p>Uzticamība un augsta kvalitāte</p>
+                                    <button class="btn btn-primary">Uz produkciju</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="...">
+
+                    <div class="carousel-item"
+                        style="background-image:url({{ asset('images/placeholder-tent-1.jpg') }})">
+                        <div class="container h-100 text-break">
+                            <div class="row mx-sm-5 h-100 align-items-center">
+                                <div class="col mx-sm-5 text-sm-start text-center">
+                                    <h1>Test 3 super long text that it shoudl technically break the layout, but
+                                        hopefully not!</h1>
+                                    <p>Uzticamība un augsta kvalitāte</p>
+                                    <button class="btn btn-primary">Uz produkciju</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+                <button class="carousel-control-prev" type="button" data-bs-target="#carousel-autoplay"
                     data-bs-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+                <button class="carousel-control-next" type="button" data-bs-target="#carousel-autoplay"
                     data-bs-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Next</span>
                 </button>
             </div>
+
         </div>
     </div>
 </div>
