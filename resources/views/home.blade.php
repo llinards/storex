4ba0a82dd73add3f0bf80ef37ad05d622af5816f
KarlisEmbergs <karlisembergs@gmail.com>
2024-11-06 20:42:03 +0200

    Updated file location. Updated why-choose-us-mob button ids

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
 <x-layout.app>
     <div id="first-section">
-        <x-layout.carousel></x-layout.carousel>
+        <x-carousel></x-carousel>
         <div class="d-none d-sm-block container mt-5 px-0">
-            <x-layout.why-choose-us></x-layout.why-choose-us>
+            <x-why-choose-us></x-why-choose-us>
         </div>
         <div class="d-block d-sm-none container mt-5 px-0">
-            <x-layout.why-choose-us-mob></x-layout.why-choose-us-mob>
+            <x-why-choose-us-mob></x-why-choose-us-mob>
         </div>
     </div>
 </x-layout.app>
