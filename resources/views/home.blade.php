68fefa09c4b31cc83ed3743da6b9179b15b02964
KarlisEmbergs <karlisembergs@gmail.com>
2024-11-06 23:19:57 +0200

    Added why choose us section with owners. Some additional css.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,4 +8,7 @@
             <x-why-choose-us-mob></x-why-choose-us-mob>
         </div>
     </div>
+    <div class="container d-none d-sm-block" id="third-section">
+        <x-why-choose-us-text></x-why-choose-us-text>
+    </div>
 </x-layout.app>
