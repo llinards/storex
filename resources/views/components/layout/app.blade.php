07f76dcc0e322ecfea46f033e3b71af00bea3a67
KarlisEmbergs <karlisembergs@gmail.com>
2024-12-03 21:48:53 +0200

    Reestablish Myriad Font

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -1,27 +1,29 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}" />
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-        <!-- Bootstrap Icons -->
-        <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    </head>
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css" />
 
-    <body>
-        @include('includes.navbar')
-        <main>
-            {{ $slot }}
-        </main>
-        @include('includes.footer')
-        <script src="../node_modules/flowbite/dist/flowbite.min.js"></script>
-    </body>
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    @include('includes.navbar')
+    <main>
+        {{ $slot }}
+    </main>
+    @include('includes.footer')
+    <script src="../node_modules/flowbite/dist/flowbite.min.js"></script>
+</body>
+
 </html>
