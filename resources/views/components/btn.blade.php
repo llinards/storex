97a7366fe7fae46be12f8f769627c1ab9f620d5b
KarlisEmbergs <karlisembergs@gmail.com>
2024-12-16 20:37:35 +0200

    Add call CTA

--- resources/views/components/btn.blade.php
+++ resources/views/components/btn.blade.php
@@ -1,4 +1,10 @@
-<button class="btn rounded-lg px-5 py-3 font-bold text-white transition duration-200 hover:drop-shadow-md">
+<button
+    {{
+        $attributes->merge([
+            'class' => 'btn rounded-lg px-5 py-3 font-bold text-white transition duration-200 hover:drop-shadow-md',
+        ])
+    }}
+>
     <a {{ $attributes }}>
         {{ $slot }}
     </a>
