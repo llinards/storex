c810fbe06bbc62f7ea4a590dc9aab4244938880e
KarlisEmbergs <karlisembergs@gmail.com>
2025-01-03 18:52:06 +0200

    Create About Us Blade

--- resources/views/components/why-choose-us/owners.blade.php
+++ resources/views/components/why-choose-us/owners.blade.php
@@ -1,24 +1,29 @@
 <div class="bg-storex-medium-grey-bg px-4 py-8 sm:px-20 sm:py-12">
-    <div class="items-center gap-20 lg:grid lg:grid-cols-2">
-        <img src="{{ asset('images/storex-owners.jpg') }}" alt="Storex Owners" class="max-h-full" />
-        <div>
-            <h2 class="pb-2 pt-4 text-center text-storex-red md:text-left lg:pt-0">
+    <div class="grid items-center lg:grid-cols-2 lg:gap-20">
+        <img
+            class="order-2 pt-4 lg:order-1 lg:pt-0"
+            src="{{ asset('images/storex-owners.jpg') }}"
+            alt="Storex Owners"
+            class="max-h-full"
+        />
+        <div class="order-1 lg:order-2">
+            <h2 class="pb-2 text-left text-storex-red">
                 @lang('Kāpēc izvēlēties STOREX angāra iegādei?')
             </h2>
-            <p class="py-2 md:text-xl">
+            <p class="py-2">
                 @lang('Mums ir vairāk nekā 10 gadu pieredze darbā ar tenta angāriem. Vairāk nekā 1000 uzņēmumu un
                 privātpersonu ir kļuvuši par mūsu klientiem ne tikai Latvijā, bet arī Eiropas valstīs. Mēs
                 piedāvājam kvalitatīvus tenta angārus, kas tiek ražoti pēc augstākajiem kvalitātes standartiem
                 EN 1090-1:2009+A1:2011 un atbilst visām ES prasībām. Angāriem ir CE sertifikāti, kas ir
                 piemēroti ES atbalsta saņemšanai.')
             </p>
-            <p class="py-2 md:text-xl">
+            <p class="py-2">
                 @lang('Mēs piedāvājam ne tikai augstākās kvalitātes angārus, bet arī izcilu klientu apkalpošanu. Jūs
                 būsiet pilnībā apmierināti ar savu pirkumu, un ja Jums radīsies kādi jautājumi vai būs
                 nepieciešama palīdzība, mūsu pieredzējušie speciālisti vienmēr būs pieejami bezmaksas
                 konsultācijām.')
             </p>
-            <p class="pt-2 md:text-xl">
+            <p class="pt-2">
                 @lang('Mūsu prioritāte ir apmierināts klients, kurš saņem tieši to, kas viņam nepieciešams –
                 kvalitatīvu risinājumu, kas pilnībā atbilst viņa vajadzībām.')
             </p>
