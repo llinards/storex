6dba8988866c5a64d273a2e43d38e502f3581fb8
KarlisEmbergs <karlisembergs@gmail.com>
2025-01-04 23:52:56 +0200

    Created Contacts view

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,5 +1,101 @@
 <x-layout.app>
-    <div class="container">
-        <h1>Hello from the Contacts</h1>
+    <div class="container mx-auto px-4 sm:py-12 lg:px-6 xl:px-8">
+        <div class="pt-28 sm:pt-0 md:grid md:grid-cols-2 md:gap-10">
+            <div>
+                <p class="pt-2 text-storex-inactive-grey">@lang('Kontakti')</p>
+                <h1 class="leading-none">@lang('Kontakti')</h1>
+                <div>
+                    <div class="py-4">
+                        <p class="text-base text-storex-red">@lang('SIA "AE Constructions"')</p>
+                        <p class="text-base">
+                            @lang('Reģistrācijas Nr.')
+                            LV44103147863
+                        </p>
+                    </div>
+                    <div class="grid content-between md:order-none">
+                        <ul class="pb-4 md:pb-0">
+                            <li>
+                                <a
+                                    href="https://maps.app.goo.gl/E9EtPST5AXgxRduN8"
+                                    class="flex items-center text-sm md:text-lg"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="icon mr-2 h-9 w-9 shrink-0"
+                                        viewBox=" 0 0 256 256"
+                                    >
+                                        <path
+                                            d="M128,64a40,40,0,1,0,40,40A40,40,0,0,0,128,64Zm0,64a24,24,0,1,1,24-24A24,24,0,0,1,128,128Zm0-112a88.1,88.1,0,0,0-88,88c0,31.4,14.51,64.68,42,96.25a254.19,254.19,0,0,0,41.45,38.3,8,8,0,0,0,9.18,0A254.19,254.19,0,0,0,174,200.25c27.45-31.57,42-64.85,42-96.25A88.1,88.1,0,0,0,128,16Zm0,206c-16.53-13-72-60.75-72-118a72,72,0,0,1,144,0C200,161.23,144.53,209,128,222Z"
+                                        ></path>
+                                    </svg>
+                                    <span class="text-base transition-all duration-200 hover:text-storex-red">
+                                        "Rožulejas", Plācis, Straupes pag.,
+                                        <br class="md:hidden" />
+                                        Pārgaujas nov., LV-4152
+                                    </span>
+                                </a>
+                            </li>
+                            <li class="flex items-center text-sm md:text-lg">
+                                <a href="tel:+37122338346" class="flex items-center text-sm md:text-lg">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="icon mr-2 h-9 w-9 shrink-0"
+                                        viewBox=" 0 0 256 256"
+                                    >
+                                        <path
+                                            d="M222.37,158.46l-47.11-21.11-.13-.06a16,16,0,0,0-15.17,1.4,8.12,8.12,0,0,0-.75.56L134.87,160c-15.42-7.49-31.34-23.29-38.83-38.51l20.78-24.71c.2-.25.39-.5.57-.77a16,16,0,0,0,1.32-15.06l0-.12L97.54,33.64a16,16,0,0,0-16.62-9.52A56.26,56.26,0,0,0,32,80c0,79.4,64.6,144,144,144a56.26,56.26,0,0,0,55.88-48.92A16,16,0,0,0,222.37,158.46ZM176,208A128.14,128.14,0,0,1,48,80,40.2,40.2,0,0,1,82.87,40a.61.61,0,0,0,0,.12l21,47L83.2,111.86a6.13,6.13,0,0,0-.57.77,16,16,0,0,0-1,15.7c9.06,18.53,27.73,37.06,46.46,46.11a16,16,0,0,0,15.75-1.14,8.44,8.44,0,0,0,.74-.56L168.89,152l47,21.05h0s.08,0,.11,0A40.21,40.21,0,0,1,176,208Z"
+                                        ></path>
+                                    </svg>
+                                    <span class="pr-1 text-base transition-all duration-200 hover:text-storex-red">
+                                        +371 22338346
+                                    </span>
+                                    <a
+                                        class="border-l-1 border-storex-grey pl-1 text-base transition-all duration-200 hover:text-storex-red"
+                                        href="tel:+37128354035"
+                                    >
+                                        +371 28354035
+                                    </a>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="mailto:info@storex.lv" class="flex items-center text-sm md:text-lg">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="icon mr-2 h-9 w-9 shrink-0"
+                                        viewBox=" 0 0 256 256"
+                                    >
+                                        <path
+                                            d="M224,48H32a8,8,0,0,0-8,8V192a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V56A8,8,0,0,0,224,48Zm-96,85.15L52.57,64H203.43ZM98.71,128,40,181.81V74.19Zm11.84,10.85,12,11.05a8,8,0,0,0,10.82,0l12-11.05,58,53.15H52.57ZM157.29,128,216,74.18V181.82Z"
+                                        ></path>
+                                    </svg>
+                                    <span class="text-base transition-all duration-200 hover:text-storex-red">
+                                        info@storex.lv
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4303.498301963428!2d24.97263110201265!3d57.36331613106734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46ebfde7456a8809%3A0xf1a55da9606c483c!2sPl%C4%81cis%2C%20Straupe%20Parish%2C%20C%C4%93sis%20Municipality%2C%20LV-4152!5e0!3m2!1sen!2slv!4v1736020342745!5m2!1sen!2slv"
+                    width="100%"
+                    height="450"
+                    style="border: 0"
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                ></iframe>
+            </div>
+        </div>
+    </div>
+    <div style="background-image: url('{{ asset('images/storex-background.png') }}')" class="bg-cover bg-center">
+        <div class="container mx-auto px-4 py-8 sm:py-12 lg:px-6 xl:px-8">
+            <x-contact-us></x-contact-us>
+        </div>
+    </div>
+    <div class="container mx-auto px-4 py-8 sm:py-12 lg:px-6 xl:px-8">
+        <x-countries></x-countries>
     </div>
 </x-layout.app>
