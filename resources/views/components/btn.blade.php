6b767a2f7a29a7ffb5cb9d6414067a3fc9afc276
KarlisEmbergs <karlisembergs@gmail.com>
2025-01-08 19:15:46 +0200

    Slight refactoring for the production button.

--- resources/views/components/btn.blade.php
+++ resources/views/components/btn.blade.php
@@ -2,7 +2,7 @@
     {{
         $attributes->merge([
             'class' => 'btn rounded-lg px-5 py-3 font-bold text-white transition duration-200 hover:drop-shadow-md
-                    ring-transparent focus:ring-transparent',
+                            ring-transparent focus:ring-transparent',
             'style' => '--tw-ring-color: #ff0000;',
         ])
     }}
