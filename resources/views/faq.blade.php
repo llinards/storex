cd82b60dbcc45112cf54d3636d59040da77d2cc9
Kārlis Embergs <karlisembergs@gmail.com>
2025-01-11 17:59:38 +0200

    Add FAQ page heading spacing

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="title">@lang('Biežāk uzdotie jautājumi')</x-slot>
     <div class="container mx-auto px-4 sm:py-12 lg:px-6 xl:px-8">
         {{-- pb-10 instead of pb-12 since Qs have py-2 --}}
-        <div class="pt-28 sm:pb-10 sm:pt-0">
+        <div class="pb-6 pt-28 sm:pb-10 sm:pt-0">
             <h1 class="leading-none">@lang('Biežāk uzdotie jautājumi')</h1>
         </div>
         <div class="mx-auto w-full pb-8 sm:pb-12">
