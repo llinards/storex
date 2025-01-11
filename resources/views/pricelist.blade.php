443f9b5e7593e9082e957b2636aa4082edea019e
Kārlis Embergs <karlisembergs@gmail.com>
2025-01-11 22:39:12 +0200

    Dynamic field renaming for pricelist

--- resources/views/pricelist.blade.php
+++ resources/views/pricelist.blade.php
@@ -8,79 +8,29 @@
     <div class="container mx-auto px-4 py-8 sm:py-12 lg:px-6 xl:px-8">
         <x-product.pricelist-wrapper>
             <x-product.entry>
-                <x-slot name="productName">NORDA 55</x-slot>
-                <x-slot name="productLength">10</x-slot>
-                <x-slot name="productWidth">5,5</x-slot>
-                <x-slot name="productHeight">5,3</x-slot>
-                <x-slot name="productArchDistance">2</x-slot>
-                <x-slot name="productGateDimension">3,4* x 4,1</x-slot>
-                <x-slot name="productArea">55</x-slot>
-                <x-slot name="productPvc">650</x-slot>
-                <x-slot name="productBlueprint">product/1</x-slot>
-                <x-slot name="productPrice">3100€</x-slot>
+                <x-slot name="name">NORDA 55</x-slot>
+                <x-slot name="length">10</x-slot>
+                <x-slot name="width">5,5</x-slot>
+                <x-slot name="height">5,3</x-slot>
+                <x-slot name="archDistance">2</x-slot>
+                <x-slot name="gateDimension">3,4* x 4,1</x-slot>
+                <x-slot name="area">55</x-slot>
+                <x-slot name="pvc">650</x-slot>
+                <x-slot name="blueprint">product/1</x-slot>
+                <x-slot name="price">3100€</x-slot>
             </x-product.entry>
 
             <x-product.entry>
-                <x-slot name="productName">NORDA 110</x-slot>
-                <x-slot name="productLength">20</x-slot>
-                <x-slot name="productWidth">5,5</x-slot>
-                <x-slot name="productHeight">5,3</x-slot>
-                <x-slot name="productArchDistance">2</x-slot>
-                <x-slot name="productGateDimension">3,4* x 4,1</x-slot>
-                <x-slot name="productArea">110</x-slot>
-                <x-slot name="productPvc">650</x-slot>
-                <x-slot name="productBlueprint">product/2</x-slot>
-                <x-slot name="productPrice">6200€</x-slot>
-            </x-product.entry>
-            <x-product.entry>
-                <x-slot name="productName">NORDA 55</x-slot>
-                <x-slot name="productLength">10</x-slot>
-                <x-slot name="productWidth">5,5</x-slot>
-                <x-slot name="productHeight">5,3</x-slot>
-                <x-slot name="productArchDistance">2</x-slot>
-                <x-slot name="productGateDimension">3,4* x 4,1</x-slot>
-                <x-slot name="productArea">55</x-slot>
-                <x-slot name="productPvc">650</x-slot>
-                <x-slot name="productBlueprint">product/3</x-slot>
-                <x-slot name="productPrice">3100€</x-slot>
-            </x-product.entry>
-
-            <x-product.entry>
-                <x-slot name="productName">NORDA 110</x-slot>
-                <x-slot name="productLength">20</x-slot>
-                <x-slot name="productWidth">5,5</x-slot>
-                <x-slot name="productHeight">5,3</x-slot>
-                <x-slot name="productArchDistance">2</x-slot>
-                <x-slot name="productGateDimension">3,4* x 4,1</x-slot>
-                <x-slot name="productArea">110</x-slot>
-                <x-slot name="productPvc">650</x-slot>
-                <x-slot name="productBlueprint">product/4</x-slot>
-                <x-slot name="productPrice">6200€</x-slot>
-            </x-product.entry>
-            <x-product.entry>
-                <x-slot name="productName">NORDA 55</x-slot>
-                <x-slot name="productLength">10</x-slot>
-                <x-slot name="productWidth">5,5</x-slot>
-                <x-slot name="productHeight">5,3</x-slot>
-                <x-slot name="productArchDistance">2</x-slot>
-                <x-slot name="productGateDimension">3,4* x 4,1</x-slot>
-                <x-slot name="productArea">55</x-slot>
-                <x-slot name="productPvc">650</x-slot>
-                <x-slot name="productBlueprint">product/5</x-slot>
-                <x-slot name="productPrice">3100€</x-slot>
-            </x-product.entry>
-
-            <x-product.entry>
-                <x-slot name="productName">NORDA 110</x-slot>
-                <x-slot name="productLength">20</x-slot>
-                <x-slot name="productWidth">5,5</x-slot>
-                <x-slot name="productHeight">5,3</x-slot>
-                <x-slot name="productArchDistance">2</x-slot>
-                <x-slot name="productGateDimension">3,4* x 4,1</x-slot>
-                <x-slot name="productArea">110</x-slot>
-                <x-slot name="productPvc">650</x-slot>
-                <x-slot name="productBlueprint">product/6</x-slot>
-                <x-slot name="productPrice">6200€</x-slot>
+                <x-slot name="name">NORDA 110</x-slot>
+                <x-slot name="length">20</x-slot>
+                <x-slot name="width">5,5</x-slot>
+                <x-slot name="height">5,3</x-slot>
+                <x-slot name="archDistance">2</x-slot>
+                <x-slot name="gateDimension">3,4* x 4,1</x-slot>
+                <x-slot name="area">110</x-slot>
+                <x-slot name="pvc">650</x-slot>
+                <x-slot name="blueprint">product/2</x-slot>
+                <x-slot name="price">6200€</x-slot>
             </x-product.entry>
         </x-product.pricelist-wrapper>
     </div>
