56452171aa3d5fc5517f356ca89d4ec31044036e
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-01-14 10:15:53 +0200

    Update CategoryTest.php

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -1,34 +1,59 @@
 <?php
 
-// it('shows the category page for a valid locale and matching category locale', function () {
-//    $category = Category::factory()->create([
-//        'title' => [
-//            'lv' => 'Lv Test Title',
-//        ],
-//        'slug'  => [
-//            'lv' => 'lv-test-slug',
-//        ],
-//    ]);
-//    $response = $this->get('/lv/lv-test-slug');
-//    $response->assertStatus(200);
-//    $response->assertSee($category->title);
-// });
-//
-// it('returns 404 when category locale does not match the route locale', function () {
-//    $category = Category::factory()->create();
-//    CategoryTranslation::factory()->create([
-//        'category_id' => $category->id,
-//        'locale'      => 'en',
-//        'slug'        => 'test-slug',
-//    ]);
-//
-//    $response = $this->get('/lv/test-slug');
-//
-//    $response->assertStatus(404);
-// });
+use App\Models\Category;
+use Illuminate\Support\Facades\Storage;
 
-it('returns 404 for a valid locale but non-existent category slug', function () {
-    $response = $this->get('/lv/non-existent-slug');
+it('shows categories page for a valid locale and matching category locale (LV)', function () {
+    Storage::fake('local');
+    $category = Category::factory()->create();
+    $response = $this->get(route('category.index', 'lv'));
+    $response->assertStatus(200)->assertSee($category->title);
+});
 
+it('shows categories page for a valid locale and matching category locale (EN)', function () {
+    Storage::fake('local');
+    $category = Category::factory()->create();
+    $response = $this->get(route('category.index', 'en'));
+    $response->assertStatus(200)->assertSee($category->title);
+});
+
+it('returns to default locale if categories doesnt have a translation', function () {
+    Storage::fake('local');
+    Category::factory()->create([
+        'title'       => ['lv' => 'Latvian title'],
+        'slug'        => ['lv' => 'latvian-slug'],
+        'description' => ['lv' => 'Latvian description'],
+    ]);
+    $response = $this->get(route('category.index', 'en'));
+    $response->assertStatus(200)->assertSee('Latvian title');
+});
+
+it('shows the category page for a valid locale and matching category locale (LV)', function () {
+    Storage::fake('local');
+    $category = Category::factory()->create();
+    $response = $this->get(route('category.show', ['lv', $category->slug]));
+    $response->assertStatus(200)->assertSee($category->title);
+});
+
+it('shows the category page for a valid locale and matching category locale (EN)', function () {
+    Storage::fake('local');
+    $category = Category::factory()->create();
+    $response = $this->get(route('category.show', ['en', $category->slug]));
+    $response->assertStatus(200)->assertSee($category->title);
+});
+
+it('returns to default locale if category doesnt have a translation', function () {
+    Storage::fake('local');
+    Category::factory()->create([
+        'title'       => ['lv' => 'Latvian title'],
+        'slug'        => ['lv' => 'latvian-slug'],
+        'description' => ['lv' => 'Latvian description'],
+    ]);
+    $response = $this->get(route('category.show', ['en', 'latvian-slug']));
+    $response->assertStatus(200)->assertSee('Latvian title');
+});
+
+it('returns 404 if not matching category was found', function () {
+    $response = $this->get(route('category.show', ['lv', 'non-existing-slug']));
     $response->assertStatus(404);
 });
