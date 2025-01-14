e11eb2dd9568cb95b5949cadd1c06ea7ec40b4df
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-01-14 13:24:01 +0200

    Update GeneralTest.php

--- tests/Feature/GeneralTest.php
+++ tests/Feature/GeneralTest.php
@@ -11,3 +11,17 @@ it('redirects to the default locale', function () {
 
     $response->assertRedirect('/lv');
 });
+
+it('returns correct locale when changing the locale (from lv to en)', function () {
+    $response = $this->get('/lv?changeLanguage=en');
+
+    $response->assertStatus(200);
+    expect(app()->getLocale())->toBe('en');
+});
+
+it('returns correct locale when changing the locale (from en to lv)', function () {
+    $response = $this->get('/en?changeLanguage=lv');
+
+    $response->assertStatus(200);
+    expect(app()->getLocale())->toBe('lv');
+});
