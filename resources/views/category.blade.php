eb2e06ee5e3aeb5b81ef232c80840ed782a70fed
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-01-21 22:12:02 +0200

    Remove is accesory check

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -25,12 +25,10 @@
                                 {{ asset('storage/products/' . $product->images[0]->filename) }}
                             </x-slot>
                             <x-slot name="heading">{{ $product->title }}</x-slot>
-                            @if(!$product->is_accessory)
-                                <x-slot name="area">0</x-slot>
-                                <x-slot name="width">0</x-slot>
-                                <x-slot name="height">0</x-slot>
-                                <x-slot name="length">0</x-slot>
-                            @endif
+                            <x-slot name="area">0</x-slot>
+                            <x-slot name="width">0</x-slot>
+                            <x-slot name="height">0</x-slot>
+                            <x-slot name="length">0</x-slot>
                             <x-slot name="link">@lang('Uzzināt vairāk')</x-slot>
                         </x-category.card>
                     @endforeach
@@ -55,12 +53,10 @@
                                 {{ asset('storage/products/' . $product->images[0]->filename) }}
                             </x-slot>
                             <x-slot name="heading">{{ $product->title }}</x-slot>
-                            @if(!$product->is_accessory)
-                                <x-slot name="area">0</x-slot>
-                                <x-slot name="width">0</x-slot>
-                                <x-slot name="height">0</x-slot>
-                                <x-slot name="length">0</x-slot>
-                            @endif
+                            <x-slot name="area">0</x-slot>
+                            <x-slot name="width">0</x-slot>
+                            <x-slot name="height">0</x-slot>
+                            <x-slot name="length">0</x-slot>
                             <x-slot name="link">@lang('Uzzināt vairāk')</x-slot>
                         </x-category.card>
                     @endforeach
