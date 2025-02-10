468c9e81f2c12880986a3914c82b8c5c7d31236d
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-02-10 21:15:00 +0200

    Update FAQ answers

--- resources/views/components/faq/content.blade.php
+++ resources/views/components/faq/content.blade.php
@@ -1,97 +1,83 @@
 <x-faq.question>
     <x-slot name="question">@lang('Kāda garantija ir Jūsu produkcijai?')</x-slot>
     <x-slot name="answer">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum dicta quisquam velit eligendi nihil,
-        perspiciatis ex nam laboriosam atque minus voluptatum consequatur ad quis nulla perferendis illum optio illo
-        aperiam.
+        @lang('3 gadu standarta garantija, garantija nestrādā uz mehāniskiem bojājumiem.')
     </x-slot>
 </x-faq.question>
 
 <x-faq.question>
     <x-slot name="question">@lang('Vai iespējams salabot tenta angāru?')</x-slot>
     <x-slot name="answer">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga nulla perferendis quidem nihil illo porro
-        consequatur nostrum. Voluptates est perferendis repellat quidem deleniti amet labore, nisi asperiores error,
-        totam magnam!
+      @lang('Tenta angāru tentu ir iespējams salabot nenoņemot to no metāla konstrukcijas (Tentu remkomplekts jeb tenta sakausēšana/salīmēšana).')
     </x-slot>
 </x-faq.question>
 
 <x-faq.question>
     <x-slot name="question">@lang('Kāds ir tenta kalpošanas laiks?')</x-slot>
     <x-slot name="answer">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima dolore laudantium quia totam reprehenderit
-        nesciunt sequi, praesentium quas sed, adipisci eum facilis eos doloremque. Reprehenderit magnam reiciendis
-        accusamus debitis fugiat?
+       @lang('Tenta kalpošanas laiks ir no 5 - 25 gadi (atkarīgs no tenta biezuma un no tā, kā tiek ekspluatēts pats angārs).')
     </x-slot>
 </x-faq.question>
 
 <x-faq.question>
     <x-slot name="question">@lang('No kāda materiāla ir taisīts angāra rāmis?')</x-slot>
     <x-slot name="answer">
-        @lang('Angāra rāmis ir veidots no cinkotas metāla trubas (diametrs ir 60cm)')
-        .
+       @lang('Angāra rāmis ir veidots no cinkotas metāla trubas (diametrs ir 60cm).')
+    </x-slot>
+</x-faq.question>
+
+<x-faq.question>
+    <x-slot name="question">@lang('Cik angāram ir vārti?')</x-slot>
+    <x-slot name="answer">
+       @lang('Standarta aprīkojuma angāram ir divi vārti.')
     </x-slot>
 </x-faq.question>
 
 <x-faq.question>
     <x-slot name="question">@lang('Vai vajag nostiprināt angāru pie zemes?')</x-slot>
     <x-slot name="answer">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima dolore laudantium quia totam reprehenderit
-        nesciunt sequi, praesentium quas sed, adipisci eum facilis eos doloremque. Reprehenderit magnam reiciendis
-        accusamus debitis fugiat?
+       @lang('Obligāti! Katrs angārs jānostiprina pie pamatnes. Stiprinājumi ir angāra komplektācijā.')
     </x-slot>
 </x-faq.question>
 
 <x-faq.question>
     <x-slot name="question">@lang('Kāds ir augstums angāru sāna malai?')</x-slot>
     <x-slot name="answer">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima dolore laudantium quia totam reprehenderit
-        nesciunt sequi, praesentium quas sed, adipisci eum facilis eos doloremque. Reprehenderit magnam reiciendis
-        accusamus debitis fugiat?
+        @lang('ALASKA-S - 2,3м. NORDA – 4,1м. EURO – 3,5м. VEGA – 3,2м.')
     </x-slot>
 </x-faq.question>
 
 <x-faq.question>
     <x-slot name="question">@lang('Vai ir iespējams savienot divus angārus kopā?')</x-slot>
     <x-slot name="answer">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima dolore laudantium quia totam reprehenderit
-        nesciunt sequi, praesentium quas sed, adipisci eum facilis eos doloremque. Reprehenderit magnam reiciendis
-        accusamus debitis fugiat?
+        @lang('Angārus ir iespējams savienot kopā tikai ar galiem.')
     </x-slot>
 </x-faq.question>
 
 <x-faq.question>
     <x-slot name="question">@lang('Vai ir iespējams nosiltināt tikai angāru?')</x-slot>
     <x-slot name="answer">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima dolore laudantium quia totam reprehenderit
-        nesciunt sequi, praesentium quas sed, adipisci eum facilis eos doloremque. Reprehenderit magnam reiciendis
-        accusamus debitis fugiat?
+    @lang('Jā, ir iespējams nosiltināt angāru pašu spēkiem ar Styrodur®, Isover® , Fastfixx®, Rockwool®, EPS')
     </x-slot>
 </x-faq.question>
 
 <x-faq.question>
     <x-slot name="question">@lang('Vai ir iespējams iemontēt citus vārtus tenta angāros?')</x-slot>
     <x-slot name="answer">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima dolore laudantium quia totam reprehenderit
-        nesciunt sequi, praesentium quas sed, adipisci eum facilis eos doloremque. Reprehenderit magnam reiciendis
-        accusamus debitis fugiat?
+     @lang('Jā, konstrukcija atļauj iemontēt citus vārtus.')
     </x-slot>
 </x-faq.question>
 
 <x-faq.question>
     <x-slot name="question">@lang('Vai ir iespējams iemontēt papildus mazās durvis?')</x-slot>
     <x-slot name="answer">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima dolore laudantium quia totam reprehenderit
-        nesciunt sequi, praesentium quas sed, adipisci eum facilis eos doloremque. Reprehenderit magnam reiciendis
-        accusamus debitis fugiat?
+        @lang('Jā, tas ir iespējams, bet standarta komplektācijā tādu vārtu nav.')
     </x-slot>
 </x-faq.question>
 
 <x-faq.question>
     <x-slot name="question">@lang('Vai Jūsu angāriem ir ventilācija?')</x-slot>
     <x-slot name="answer">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima dolore laudantium quia totam reprehenderit
-        nesciunt sequi, praesentium quas sed, adipisci eum facilis eos doloremque. Reprehenderit magnam reiciendis
-        accusamus debitis fugiat?
+       @lang('Visiem angāriem ir ventilācijas lūkas.')
     </x-slot>
 </x-faq.question>
