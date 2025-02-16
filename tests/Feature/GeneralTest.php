a7aa9afac70dad2aa1186e4cc50b1bd63057e164
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-02-16 13:49:31 +0200

    Update GeneralTest.php

--- tests/Feature/GeneralTest.php
+++ tests/Feature/GeneralTest.php
@@ -9,14 +9,25 @@ it('returns a successful response', fn() => $this->get('/lv')->assertStatus(200)
 
 it('redirects to the default locale', fn() => $this->get('/')->assertRedirect('/lv'));
 
-it('returns a page with all products', fn() => $this->get(route('category.index', 'lv'))->assertStatus(200));
+it('returns a page with all products', function () {
+    $category = Category::factory()->create();
+    $product  = Product::factory()->create(['category_id' => $category->id]);
+    ProductImage::factory()->create(['product_id' => $product->id]);
+    $this->get(route('category.index', 'lv'))
+         ->assertStatus(200)
+         ->assertSee($category->name)
+         ->assertSee($product->name);
+});
 
 it('returns a page of products belonging to a category', function () {
     Storage::fake('local');
     $category = Category::factory()->create();
     $product  = Product::factory()->create(['category_id' => $category->id]);
-    ProductImage::factory(1)->create(['product_id' => $product->id]);
-    $this->get(route('category.show', ['lv', $category->slug]))->assertStatus(200);
+    ProductImage::factory()->create(['product_id' => $product->id]);
+    $this->get(route('category.show', ['lv', $category->slug]))
+         ->assertStatus(200)
+         ->assertSee($category->name)
+         ->assertSee($product->name);
 });
 
 it('returns a detailed page of product', function () {
@@ -24,7 +35,10 @@ it('returns a detailed page of product', function () {
     $category = Category::factory()->create();
     $product  = Product::factory()->create(['category_id' => $category->id]);
     ProductImage::factory(1)->create(['product_id' => $product->id]);
-    $this->get(route('product.show', ['lv', $category->slug, $product->slug]))->assertStatus(200);
+    $this->get(route('product.show', ['lv', $category->slug, $product->slug]))
+         ->assertStatus(200)
+         ->assertSee($category->name)
+         ->assertSee($product->name);
 });
 
 it('returns a page with a pricelist', fn() => $this->get(route('pricelist', 'lv'))->assertStatus(200));
