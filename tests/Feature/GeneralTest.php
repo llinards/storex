889141c4ab739e319f34b4399fead87a3d8221cd
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-02-16 13:40:41 +0200

    Update general tests

--- tests/Feature/GeneralTest.php
+++ tests/Feature/GeneralTest.php
@@ -1,27 +1,38 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/lv');
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\ProductImage;
+use Illuminate\Support\Facades\Storage;
 
-    $response->assertStatus(200);
+it('returns a successful response', fn() => $this->get('/lv')->assertStatus(200));
+
+it('redirects to the default locale', fn() => $this->get('/')->assertRedirect('/lv'));
+
+it('returns a page with all products', fn() => $this->get(route('category.index', 'lv'))->assertStatus(200));
+
+it('returns a page of products belonging to a category', function () {
+    Storage::fake('local');
+    $category = Category::factory()->create();
+    $product  = Product::factory()->create(['category_id' => $category->id]);
+    ProductImage::factory(1)->create(['product_id' => $product->id]);
+    $this->get(route('category.show', ['lv', $category->slug]))->assertStatus(200);
 });
 
-it('redirects to the default locale', function () {
-    $response = $this->get('/');
-
-    $response->assertRedirect('/lv');
+it('returns a detailed page of product', function () {
+    Storage::fake('local');
+    $category = Category::factory()->create();
+    $product  = Product::factory()->create(['category_id' => $category->id]);
+    ProductImage::factory(1)->create(['product_id' => $product->id]);
+    $this->get(route('product.show', ['lv', $category->slug, $product->slug]))->assertStatus(200);
 });
 
-it('returns correct locale when changing the locale (from lv to en)', function () {
-    $response = $this->get('/lv?changeLanguage=en');
+it('returns a page with a pricelist', fn() => $this->get(route('pricelist', 'lv'))->assertStatus(200));
 
-    $response->assertStatus(200);
-    expect(app()->getLocale())->toBe('en');
-});
+it('returns a faq page', fn() => $this->get(route('faq', 'lv'))->assertStatus(200));
 
-it('returns correct locale when changing the locale (from en to lv)', function () {
-    $response = $this->get('/en?changeLanguage=lv');
+it('returns a contact us page', fn() => $this->get(route('contacts', 'lv'))->assertStatus(200));
 
-    $response->assertStatus(200);
-    expect(app()->getLocale())->toBe('lv');
-});
+it('returns an about us page', fn() => $this->get(route('about', 'lv'))->assertStatus(200));
+
+it('returns a privacy policy page', fn() => $this->get(route('privacy-policy', 'lv'))->assertStatus(200));
